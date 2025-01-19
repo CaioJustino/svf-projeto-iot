df5d08e8298772078a4d77b09437105b37b5855f
Caio V. A. Justino <85230534+CaioJustino@users.noreply.github.com>
2025-01-19 17:11:39 -0300

    Update SistemaValidacaoDeFrequencia.ino

--- SistemaValidacaoDeFrequencia.ino
+++ SistemaValidacaoDeFrequencia.ino
@@ -1,19 +1,33 @@
+/******************************************************************
+- Vcc ) 3V3 (ou Vin(5V), dependendo da versão do módulo)
+- D14 ) RST (Reset)
+- GND ) GND (Masse)
+- 19  ) MISO (Master Input Slave Output)
+- 23  ) MOSI (Master Output Slave Input)
+- 18  ) SCK (Serial Clock)
+- 5   ) SS/SDA (Slave select)
+******************************************************************/
+
 //Adafruit.IO
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <math.h>
+#include <SPIFFS.h>
+#include <MFRC522.h>  // Biblioteca para o RFID
 
-#define IO_USERNAME  "AndreGomes"
-#define IO_KEY       "aio_TOWw08ZtS6JHEc7InqHDqwS71jEK"
-const char* ssid = "NPITI-IoT";
-const char* password ="NPITI-IoT";
+#define RST_PIN 14
+#define SS_PIN 5
+#define IO_USERNAME  "AndreGomes" //<----------Mudar para o seu
+#define IO_KEY       "aio_IyCc72SRy2uM2hf3zSRT4ECALvnj" //<----------Mudar para o seu
+const char* ssid = "Wokwi-GUEST";
+const char* password ="";
 
 const char* mqttserver = "io.adafruit.com";
 const int mqttport = 1883;
 const char* mqttUser = IO_USERNAME;
 const char* mqttPassword = IO_KEY;
-int oldTotalFreq = 0;
-int oldLostFreq = 0;
+int oldTotalFreq = 1;
+int oldLostFreq = 1;
 bool test = true;
 
 WiFiClient espClient;
@@ -23,48 +37,41 @@ unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
-
-//SPIFFS
-
-#include <FS.h>
-#include <SPIFFS.h>
-
-
-//Configuração do Projeto
-
 //Botão Verde
-int btPin = 5;
+int btPin = 2;
 int leitura = 0;
 
 //Botão Vermelho
 int btPin_2 = 4;
-unsigned long lastDebounceTime = 0; //Tempo do último debounce
-unsigned long debounceDelay = 50;  //Delay para debounce
+unsigned long lastDebounceTime = 0; // Tempo do último debounce
+unsigned long debounceDelay = 50;  // Delay para debounce
 bool lastButtonState = LOW; //Último "Estado" do Botão
 bool currentButtonState = LOW; //Atual "Estado" do Botão
 
 //Define as entradas do Sensor de distância ultrassônico HC-SR04
-#define PIN_TRIG 33
-#define PIN_ECHO 32
+#define PIN_TRIG 12
+#define PIN_ECHO 14
 
 //Define os LEDs
-#define GREEN 25
-#define YELL 26
-#define RED 27
+#define Green 33
+#define Red 25
+#define Blue 32
+
+//RFID
+#define RST_PIN 14
+#define SS_PIN 5
 
 //Dados da Frequência
 int freq1 = 0;
 int freq2 = 0;
-int totalFreq = 0;
-int lostFreq = 0;
-int validaFreq = lostFreq + totalFreq;
+int rFreq = 10;
+
 int tFreq = 0; //Tempo de Aula decorrido
 int tAula = 240; //Tempo de Aula em segundos
 
 int oldSec = 0; //Anti-bounce para o Tempo de Aula
 int r = random(0, 40); //Valor Aleatório para a Frequência
 bool pausado = false; //Indica se está pausado
-bool oldPausado = false;
 
 //Contadores e Verificadores
 unsigned long cont_tFreq = 0; //Tempo total
@@ -72,13 +79,63 @@ unsigned long tempoSalvo = 0; //Tempo salvo ao pausar
 int c = 0; //Contador para a Finalização da Aula
 
 bool pausaPorDistancia = false; //Verificador de Pausa por Distância
+bool autoFreq = false;
+int oldPausado = 1;
+int totalFreq = 0;
+int lostFreq = 0;
 
+int intervaloFreq = 30;
+int defineFreq = 4;
+int atualFreq = 0;
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+// Variáveis para SPIFFS
+String filename = "/config.txt";
+
+// Função para salvar o estado no SPIFFS
+void saveState(int tFreq, int tAula, unsigned long tempoSalvo, int sLost, int sFreq) {
+  File file = SPIFFS.open(filename, FILE_WRITE);
+  if (file) {
+    file.println(String(tFreq));
+    file.println(String(tAula));
+    file.println(String(tempoSalvo));
+    file.println(String(sLost));
+    file.println(String(sFreq));
+    file.close();
+    Serial.println("Estado salvo no SPIFFS.");
+  } else {
+    Serial.println("Erro ao salvar o estado no SPIFFS.");
+  }
+}
+
+// Função para carregar o estado do SPIFFS
+void loadState() {
+  File file = SPIFFS.open(filename, FILE_READ);
+  if (file) {
+    tFreq = file.readStringUntil('\n').toInt();
+    tAula = file.readStringUntil('\n').toInt();
+    tempoSalvo = file.readStringUntil('\n').toULong();
+    sLost = file.readStringUntil('\n').toInt();
+    sFreq = file.readStringUntil('\n').toInt();
+    file.close();
+    Serial.println("Estado carregado do SPIFFS.");
+  } else {
+    Serial.println("Arquivo de estado não encontrado. Iniciando com valores padrão.");
+    tFreq = 0;
+    tAula = 240;
+    tempoSalvo = 0;
+    sLost = 0;
+    sFreq = 0;
+  }
+}
 
 //Adafruit.IO - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-void setup_wifi() {
+const char* feedTfreq = "AndreGomes/feeds/tFreq"; // Nome do feed tFreq
 
+void setup_wifi() {
   delay(10);
- 
+
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -93,40 +150,49 @@ void setup_wifi() {
 
   randomSeed(micros());
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Messagem recebida [");
+  Serial.print("Mensagem recebida [");
   Serial.print(topic);
   Serial.print("] ");
-  String messageTemp;
   
+  String messageTemp;
+
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-    //messageTemp += (char)payload[i]; <----------Usar quando tiver uma mensagem na resposta do bloco
+    messageTemp += (char)payload[i];
   }
-  Serial.println();
+  Serial.println(messageTemp);
+
+  // Verifica se a mensagem pertence ao feed tFreq
+  if (String(topic) == feedTfreq) {
+    tFreq = messageTemp.toInt(); // Converte a mensagem para inteiro e armazena em tFreq
+    Serial.print("tFreq atualizado: ");
+    Serial.println(tFreq);
+  }
 }
 
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Tentando conexão MQTT...");
-    
-    //Cria um client ID aleatório
-    String clientId = "ESP32 - Sensores";
+    String clientId = "ESP32-Sensores-";
     clientId += String(random(0xffff), HEX);
-    
+
     if (client.connect(clientId.c_str(), mqttUser, mqttPassword)) {
-      Serial.println("conectado");
-      
+      Serial.println("Conectado ao MQTT");
+
+      // Publica uma mensagem inicial
       client.publish("AndreGomes/feeds/time-ui", "Iniciando Comunicação");
       client.publish("AndreGomes/feeds/userinwork", "Iniciando Comunicação");
       client.publish("AndreGomes/feeds/lostfreq", "Iniciando Comunicação");
+      client.publish("AndreGomes/feeds/rFreq", "Iniciando Comunicação");
 
+      // Faz o subscribe ao feed tFreq
+      client.subscribe(feedTfreq);
+      Serial.println("Inscrito no feed tFreq");
     } else {
       Serial.print("Falha, rc=");
       Serial.print(client.state());
@@ -138,55 +204,7 @@ void reconnect() {
 //Adafruit.IO - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 
-//ISppifsTime - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-void writeFile(String state, String path) {
-  File rFile = SPIFFS.open(path, "w");
-  if (!rFile) {
-    Serial.println("Erro ao abrir arquivo para escrita!");
-    return;
-  }
-  if (rFile.print(state)) {
-    Serial.print("Gravou: ");
-    Serial.println(state);
-  } else {
-    Serial.println("Erro ao gravar no arquivo!");
-  }
-  rFile.close();
-}
-
-//Função que lê o código já gravado no ESP no endereço especificado
-String readFile(String path) {
-  File rFile = SPIFFS.open(path, "r");
-  if (rFile) {
-    String s = rFile.readStringUntil('\n');
-    s.trim();
-    Serial.print("Lido: ");
-    Serial.println(s);
-    rFile.close();
-    return s;
-  } else {
-    Serial.println("Erro ao abrir arquivo!");
-    return "";
-  }
-}
-
-//Função que inicia o protocolo
-void openFS() {
-  if (SPIFFS.begin()) {
-    Serial.println("Sistema de arquivos aberto com sucesso!");
-  } else {
-    Serial.println("Erro ao abrir o sistema de arquivos");
-  }
-}
-
-String strSppifsTime, readSSppifsTime;
-unsigned long sppifsTime = 0;
-unsigned long millisAdjustedTIME = 5000;
-
-//ISppifsTime - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-
-//Função para Formatar o Tempo
+//Formatar o Tempo
 String formatTime(unsigned long time) {
   int minutes = time / 60000;
   int seconds = (time % 60000) / 1000;
@@ -195,16 +213,21 @@ String formatTime(unsigned long time) {
   return String(buffer);
 }
 
-//Função para Retirar o Delay
+//Retirar o Delay
 int formatSeconds(unsigned long time) {
   int minutes = time / 60000;
   int seconds = (time % 60000) / 1000;
   return seconds;
 }
 
-void setup() {
-  openFS();
+//Led RGB
+void corLed(int R, int G, int B) {
+    digitalWrite(Red, R);
+    digitalWrite(Green, G);
+    digitalWrite(Blue, B);
+}
 
+void setup() {
   Serial.begin(9600);
   pinMode(btPin, INPUT);
   pinMode(btPin_2, INPUT);
@@ -212,194 +235,146 @@ void setup() {
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
 
-  pinMode(GREEN, OUTPUT);
-  pinMode(YELL, OUTPUT);
-  pinMode(RED, OUTPUT); 
-
+  pinMode(Red, OUTPUT);
+  pinMode(Blue, OUTPUT);
+  pinMode(Green, OUTPUT);
+  
   setup_wifi();
-  client.setServer(mqttserver, 1883); //Publicar
-  client.setCallback(callback); //Receber mensagem
-
-  //Entrada
-  String savedTotalFreq = readFile("/totalFreq.txt");
-  if (savedTotalFreq != "") {
-    totalFreq = savedTotalFreq.toInt();
-    Serial.print("Total Frequências carregadas: ");
-    Serial.println(totalFreq);
+  client.setServer(mqttserver, 1883); // Publicar
+  client.setCallback(callback); // Receber mensagem 
+  
+  corLed(0, 254, 0);
+    // Inicializa SPIFFS
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Falha ao montar o sistema de arquivos SPIFFS.");
+    return;
   }
 
-  String savedLostFreq = readFile("/lostFreq.txt");
-  if (savedLostFreq != "") {
-    lostFreq = savedLostFreq.toInt();
-    Serial.print("Frequências Perdidas carregadas: ");
-    Serial.println(lostFreq);
-  }
+  // Carrega estado salvo
+  loadState();
 
-  //Ler o Tempo Salvo
-  if (readFile("/logstTime.txt") != "") {
-    readSSppifsTime = readFile("/logstTime.txt");
-    sppifsTime = readSSppifsTime.toInt();
+  // Inicializa WiFi
+  setup_wifi();
+  client.setServer(mqttserver, 1883);
+  client.setCallback(callback);
 
-    if (sppifsTime == 0 && readSSppifsTime != "0") {
-      sppifsTime = 0;
-    }
-  }
+  // Inicializa RFID
+  SPI.begin();
+  rfid.PCD_Init();
+  Serial.println("RFID inicializado. Aguardando conexão...");
 }
 
 void loop() {
+  
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    Serial.println("Aguardando conexão do módulo RFID...");
+    delay(1000);
+    return;
+  }
+
   if (!client.connected()) {
     reconnect();
   }
-
-  //Calcular o tempo ajustado
-  unsigned long millisAdjusted = millis() + sppifsTime;
-
-  if (millisAdjusted == millisAdjustedTIME ){
-    strSppifsTime = String(millisAdjusted);
-    writeFile(strSppifsTime, "/logstTime.txt");
-    millisAdjustedTIME += 10000;
+  
+  while (rFreq == 0){
+    client.loop();
   }
   
-  //Leitura do sensor
+  //Leitura do Sensor
   digitalWrite(PIN_TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(PIN_TRIG, LOW);
-
   //Distância
   int duration = pulseIn(PIN_ECHO, HIGH);
   int cm = duration * 0.034 / 2;
-  bool leitura_2 = digitalRead(btPin_2); //Leitura do botão vermelho
-  if (leitura_2 == true) {
-    leitura_2 = false;
-  } else {
-    leitura_2 = true;
-  }
+  bool leitura_2 = digitalRead(btPin_2); //Leitura do Botão Vermelho
 
-  //Pausar por distância
+  //Pausar por DISTÂNCIA
   if (cm <= 10 || cm >= 40) {
     if (!pausado && !pausaPorDistancia) {
-      tempoSalvo = millisAdjusted;
+      tempoSalvo = millis();
       pausado = true;
       pausaPorDistancia = true;
       Serial.println("\r\nTempo de Aula Pausado! Aproxime-se do sensor.");
-      digitalWrite(YELL, HIGH);
+      corLed(0, 254, 0);
+
     }
   } else if (pausaPorDistancia && pausado) {
-    cont_tFreq += millisAdjusted - tempoSalvo;
+    cont_tFreq += millis() - tempoSalvo;
     pausado = false;
     pausaPorDistancia = false;
-    digitalWrite(YELL, LOW);
-    Serial.println("\r\nTempo de Aula Retomado!");
+    corLed(0, 0, 0);
+    Serial.println("\r\nTempo de Aula Retomado por DISTÂNCIA!");
   }
-
-  //Pausar por botão
-  else if (cm > 10 || cm < 40) {
+  
+  //Pausar por BOTÃO
+  else if (cm > 10 || cm < 40){
     if (leitura_2 != lastButtonState) {
-      lastDebounceTime = millisAdjusted;
+      lastDebounceTime = millis();
     }
 
-    if ((millisAdjusted - lastDebounceTime) > debounceDelay) {
+    if ((millis() - lastDebounceTime) > debounceDelay) {
       if (leitura_2 != currentButtonState) {
         currentButtonState = leitura_2;
 
         if (currentButtonState == HIGH) {
           pausado = !pausado;
           if (pausado) {
-            tempoSalvo = millisAdjusted;
-            digitalWrite(YELL, HIGH);
+            tempoSalvo = millis();
+            corLed(254, 254, 0);
             Serial.println("\r\nTempo de Aula Pausado!");
           } else {
-            cont_tFreq += millisAdjusted - tempoSalvo;
-            digitalWrite(YELL, LOW);
+            cont_tFreq += millis() - tempoSalvo;
+            corLed(0, 254, 0);
             Serial.println("\r\nTempo de Aula Retomado!");
           }
         }
       }
     }
     lastButtonState = leitura_2;
+
   }
 
-  //Processos enquanto não está pausado
+  if (autoFreq == true && defineFreq != atualFreq){
+    rFreq += intervaloFreq;
+    autoFreq = false;
+  } 
+
   if (!pausado) {
-    tFreq = (millisAdjusted - cont_tFreq) / 1000; // Tempo em segundos
+    tFreq = (millis() - cont_tFreq) / 1000; // Tempo em segundos
     leitura = digitalRead(btPin);
 
-    if (leitura == true) {
-      leitura = false;
-    } else {
-      leitura = true;
-    }
-
     //1ª Frequência
-    if (tFreq > (10 + r) && tFreq <= ((10 + r) + 10) && validaFreq == 0) {
-      int resto = ((10 + r) + 10) - tFreq;
+    if (tFreq > (rFreq + r) && tFreq <= ((rFreq + r) + 10)) {
+      int resto = ((rFreq + r) + 10) - tFreq;
 
-      if (leitura == LOW && resto != tFreq && resto != 0 && freq1 == 0) {
+      if (leitura == LOW && resto != tFreq && resto != 0) {
         Serial.print("\r\nAperte o Botão Verde para validar a 1ª Frequência! Tempo restante: ");
         Serial.println(resto);
-        digitalWrite(YELL, HIGH);
+        corLed(254, 0, 254);
         delay(1000);
-        digitalWrite(YELL, LOW);
 
-      } else if (leitura == LOW && resto == 0 && freq1 == 0) {
-        lostFreq++;
+      } else if (leitura == LOW && resto == 0) {
         Serial.print("\r\nTempo Esgotado! A 1ª Frequência não foi validada.\r\n");
-        digitalWrite(RED, HIGH);
+        corLed(254, 0, 0);
         delay(1000);
-        digitalWrite(RED, LOW);
+        corLed(0, 254, 0);
+        autoFreq = true;
+        atualFreq++;
 
-      } else if (leitura == HIGH && freq1 == 0) {
-        freq1 = 1;
-        totalFreq++;
+      } else if (leitura == HIGH) {
         Serial.print("\r\nA 1ª Frequência foi validada com sucesso!\r\n");
-        digitalWrite(GREEN, HIGH);
+        corLed(0, 254, 0);
         delay(1000);
-        digitalWrite(GREEN, LOW);
+        corLed(0, 254, 0);
+        autoFreq = true;
+        atualFreq++;
 
       } else {
-        int sec = formatSeconds(millisAdjusted);
-        if (sec != oldSec) {
+        int sec = formatSeconds(millis());
+        if (sec != oldSec){
           Serial.print("\r\nTempo de Aula: ");
-          Serial.println(formatTime(millisAdjusted - cont_tFreq));
-          oldSec = sec;
-        }
-      }
-    }
-
-    //2ª Frequência
-    else if (tFreq > (70 + r) && tFreq <= ((70 + r) + 10) && validaFreq == 1) {
-      int resto = ((70 + r) + 10) - tFreq;
-
-      if (leitura == LOW && resto != tFreq && resto != 0 && freq2 == 0) {
-        Serial.print("\r\nAperte o Botão Verde para validar a 2ª Frequência! Tempo restante: ");
-        Serial.println(resto);
-        digitalWrite(YELL, HIGH);
-        delay(50);
-        digitalWrite(YELL, LOW);
-        delay(1000);
-
-      } else if (leitura == LOW && resto == 0 && freq2 == 0) {
-        lostFreq++;
-        Serial.print("\r\nTempo Esgotado! A 2ª Frequência não foi validada.\r\n");
-        digitalWrite(RED, HIGH);
-        delay(50);
-        digitalWrite(RED, LOW);
-        delay(1000);
-
-      } else if (leitura == HIGH && freq2 == 0) {
-        freq2 = 1;
-        totalFreq++;
-        Serial.print("\r\nA 2ª Frequência foi validada com sucesso!\r\n");
-        digitalWrite(GREEN, HIGH);
-        delay(50);
-        digitalWrite(GREEN, LOW);
-        delay(1000);
-
-      } else {
-        int sec = formatSeconds(millisAdjusted);
-        if (sec != oldSec) {
-          Serial.print("\r\nTempo de Aula: ");
-          Serial.println(formatTime(millisAdjusted - cont_tFreq));
+          Serial.println(formatTime(millis() - cont_tFreq));
           oldSec = sec;
         }
       }
@@ -413,63 +388,38 @@ void loop() {
         Serial.print("\r\nTotal de Frequências Cadastradas: ");
         Serial.println(freq1 + freq2);
         delay(1000);
+
       }
     } else {
-      int sec = formatSeconds(millisAdjusted);
-      if (sec != oldSec) {
+      int sec = formatSeconds(millis());
+      if (sec != oldSec){
         Serial.print("\r\nTempo de Aula: ");
-        Serial.println(formatTime(millisAdjusted - cont_tFreq));
+        Serial.println(formatTime(millis() - cont_tFreq));
         oldSec = sec;
       }
+      
     }
   }
 
-  //Publicar mensagens no MQTT
-  if (test) {
-    char sUser[8];
-    dtostrf(pausado, 1, 2, sUser);
-    client.publish("AndreGomes/feeds/userinwork", sUser);
-    char sFreq[8];
-    dtostrf(totalFreq, 1, 2, sFreq);
-    client.publish("AndreGomes/feeds/time-ui", sFreq);
-    char sLost[8];
-    dtostrf(lostFreq, 1, 2, sLost);
-    client.publish("AndreGomes/feeds/lostfreq", sLost);
+    if (pausado != oldPausado){
+      char sUser[8];
+      dtostrf(pausado,1,2,sUser);
+      client.publish("AndreGomes/feeds/userinwork", sUser);
+      oldPausado = pausado;
+    }
 
-    test = !test;
-  }
+    if (oldTotalFreq != totalFreq){
+      char sFreq[8];
+      dtostrf(totalFreq,1,2,sFreq);
+      client.publish("AndreGomes/feeds/time-ui", sFreq);
+      oldTotalFreq = totalFreq;
+    }
 
-  if (pausado != oldPausado) {
-    char sUser[8];
-    dtostrf(pausado, 1, 2, sUser);
-    client.publish("AndreGomes/feeds/userinwork", sUser);
-    oldPausado = pausado;
-    delay(1000);
-  }
-
-  if (oldTotalFreq != totalFreq) {
-    //Atualizar o valor no SPIFFS
-    writeFile(String(totalFreq), "/totalFreq.txt");
-
-    //Publicar no MQTT
-    char sFreq[8];
-    dtostrf(totalFreq, 1, 2, sFreq);
-    client.publish("AndreGomes/feeds/time-ui", sFreq);
-
-    oldTotalFreq = totalFreq;
-    delay(1000);
-  }
-
-  if (oldLostFreq != lostFreq) {
-    //Atualizar o valor no SPIFFS
-    writeFile(String(lostFreq), "/lostFreq.txt");
-
-    //Publicar no MQTT
-    char sLost[8];
-    dtostrf(lostFreq, 1, 2, sLost);
-    client.publish("AndreGomes/feeds/lostfreq", sLost);
-
-    oldLostFreq = lostFreq;
-    delay(1000);
-  }
+    if (oldLostFreq != lostFreq){
+      char sLost[8];
+      dtostrf(lostFreq,1,2,sLost);
+      client.publish("AndreGomes/feeds/lostfreq", sLost);
+      oldLostFreq = lostFreq;
+    }
+  saveState(tFreq, tAula, tempoSalvo, sLost, sFreq);
 }
